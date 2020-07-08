5c31ff9b04d850890d6d81cb883867c0a8ddd58e
Keith Millar <keithmillar@hotmail.ca>
2020-07-07 20:29:29 -0400

    useState added, can click days now

--- stories/index.js
+++ stories/index.js
@@ -36,6 +36,8 @@ storiesOf("DayListItem", module) //Initiates Storybook and registers our DayList
     <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} /> // action() allows us to create a callback that appears in the actions panel when clicked
   ));
 
+
+
   const days = [
     {
       id: 1,
@@ -54,6 +56,7 @@ storiesOf("DayListItem", module) //Initiates Storybook and registers our DayList
     },
   ];
   
+
   storiesOf("DayList", module)
     .addParameters({
       backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
