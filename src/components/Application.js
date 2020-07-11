9a52f634f1bf6f0e5081ae04f28c7d1be7edcde5
Keith Millar <keithmillar@hotmail.ca>
2020-07-11 18:17:26 -0400

    hooked up axios get request to the api server

--- src/components/Application.js
+++ src/components/Application.js
@@ -56,28 +56,39 @@ const appointments = [
   }
 ];
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+// const days = [
+//   {
+//     id: 1,
+//     name: "Monday",
+//     spots: 2,
+//   },
+//   {
+//     id: 2,
+//     name: "Tuesday",
+//     spots: 5,
+//   },
+//   {
+//     id: 3,
+//     name: "Wednesday",
+//     spots: 0,
+//   },
+// ];
+
+
 
 
 export default function Application(props) {
 const [day, setDay] = useState("Monday");
+const [days, setDays] = useState([]);
 
+useEffect(() => {
+  axios.get(`/api/days`)
+    .then(response => setDays(response.data))
+    // .then(response => console.log(response.data.results))
+    .catch(function (error) {
+      console.log(error);
+    })
+})
   return (
     <main className="layout">
       <section className="sidebar">
