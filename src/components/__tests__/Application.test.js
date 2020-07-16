82f6d3eb594843e9e02971504c4b00fd815345c5
Keith Millar <keithmillar@hotmail.ca>
2020-07-16 17:22:21 -0400

    mock data, new tests, transition to tests not relying on api calls

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,57 @@
 import React from "react";
 
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
 
 import Application from "components/Application";
+import axios from "axios";
+
 
 afterEach(cleanup);
 
-it("renders without crashing", () => {
-  render(<Application />);
+// xit("renders without crashing", () => {
+//   render(<Application />);
+// });
+
+
+// // step 1
+// it("defaults to Monday and changes the schedule when a new day is selected", () => {
+//   const { getByText } = render(<Application />);
+
+//   return waitForElement(() => getByText("Monday"));
+// });
+
+
+// // step 2
+// it("defaults to Monday and changes the schedule when a new day is selected", () => {
+//   const { getByText } = render(<Application />);
+
+//   return waitForElement(() => getByText("Monday")).then(() => {
+//     fireEvent.click(getByText("Tuesday"));
+//     expect(getByText("Leopold Silvers")).toBeInTheDocument();
+//   });
+// });
+
+// step 3
+it("changes the schedule when a new day is selected", () => {
+  const { getByText } = render(<Application />);
+
+  return waitForElement(() => getByText("Monday")).then(() => {
+    fireEvent.click(getByText("Tuesday"));
+
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  });
 });
+
+///////// ALTERNATIVE version of above, keep for reference ///////////
+
+// it("changes the schedule when a new day is selected", async () => {
+//   const { getByText } = render(<Application />);
+
+//   await waitForElement(() => getByText("Monday"));
+
+//   fireEvent.click(getByText("Tuesday"));
+
+//   expect(getByText("Leopold Silvers")).toBeInTheDocument();
+// });
+////////
+////////
