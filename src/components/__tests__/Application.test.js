824ff3c613aa198321b31f0d90d7b7cc1a24ab4c
Keith Millar <keithmillar@hotmail.ca>
2020-07-16 22:09:56 -0400

    increased fidelity of one of the application tests, now looks for text as transitions through modes

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId, getByAltText, getByPlaceholderText } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, getByText, queryByText, prettyDOM, getAllByTestId, getByAltText, getByPlaceholderText } from "@testing-library/react";
 import Application from "components/Application";
 import axios from "axios";
 
@@ -34,22 +34,58 @@ describe("Application", () => {
   ////////
   ////////
 
-  it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
-    const { container } = render(<Application />);
+  // it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
+  //   const { container, debug } = render(<Application />);
     
+  //   await waitForElement(() => getByText(container, "Archie Cohen"));
+  //   const appointment = getAllByTestId(container, "appointment")[0];
+
+  //   fireEvent.click(getByAltText(appointment, "Add"));
+
+  //   fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+  //     target: { name: "Lydia Miller-Jones" }
+  //   });
+  //   fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+  //   fireEvent.click(getByText(appointment, "Save"));
+  //   // /////
+  //   // await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
+  //   // const day = getAllByTestId(container, "day").find(day =>
+  //   //   queryByText(day, "Monday")
+  //   // );
+    
+  //   // console.log(prettyDOM(day));
+  //   // expect(getByText(day, "no spots remaining")).toBeInTheDocument();
+
+  //   console.log(prettyDOM(container));
+  // });
+
+  it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
+    const { container, debug } = render(<Application />);
+  
     await waitForElement(() => getByText(container, "Archie Cohen"));
-    const appointment = getAllByTestId(container, "appointment")[0];
-
+  
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments[0];
+  
     fireEvent.click(getByAltText(appointment, "Add"));
-
+  
     fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
-      target: { name: "Lydia Miller-Jones" }
+      target: { value: "Lydia Miller-Jones" }
     });
+  
     fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
-
     fireEvent.click(getByText(appointment, "Save"));
-
-    console.log(prettyDOM(container));
+  
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+  
+    await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
+  
+    const day = getAllByTestId(container, "day").find(day =>
+      queryByText(day, "Monday")
+    );
+  
+    expect(getByText(day, "no spots remaining")).toBeInTheDocument();
   });
 
 
