d6150a4bff4bcd7e1a83dfd3b852f85b680cae1b
Keith Millar <keithmillar@hotmail.ca>
2020-07-16 20:18:37 -0400

    test that books interview, and sees if spots remaining goes down by 1, axios.put

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -59,7 +59,7 @@ export default function Appointment(props) {
     
     <>
       <Header time={props.time} />
-      <article className="appointment">
+      <article className="appointment" data-testid="appointment">
         {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
         {mode === SHOW && (
           <Show
--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,57 +1,56 @@
 import React from "react";
-
-import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
-
+import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId, getByAltText, getByPlaceholderText } from "@testing-library/react";
 import Application from "components/Application";
 import axios from "axios";
 
-
 afterEach(cleanup);
 
-// xit("renders without crashing", () => {
-//   render(<Application />);
-// });
+describe("Application", () => {
 
+  // const appointments = getAllByTestId(container, "appointment");
+  // console.log(prettyDOM(appointments));
 
-// // step 1
-// it("defaults to Monday and changes the schedule when a new day is selected", () => {
-//   const { getByText } = render(<Application />);
+  it("changes the schedule when a new day is selected", () => {
+    const { getByText } = render(<Application />);
 
-//   return waitForElement(() => getByText("Monday"));
-// });
+    return waitForElement(() => getByText("Monday")).then(() => {
+      fireEvent.click(getByText("Tuesday"));
 
-
-// // step 2
-// it("defaults to Monday and changes the schedule when a new day is selected", () => {
-//   const { getByText } = render(<Application />);
-
-//   return waitForElement(() => getByText("Monday")).then(() => {
-//     fireEvent.click(getByText("Tuesday"));
-//     expect(getByText("Leopold Silvers")).toBeInTheDocument();
-//   });
-// });
-
-// step 3
-it("changes the schedule when a new day is selected", () => {
-  const { getByText } = render(<Application />);
-
-  return waitForElement(() => getByText("Monday")).then(() => {
-    fireEvent.click(getByText("Tuesday"));
-
-    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+      expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    });
   });
+
+  ///////// ALTERNATIVE version of above, keep for reference ///////////
+
+  // it("changes the schedule when a new day is selected", async () => {
+  //   const { getByText } = render(<Application />);
+
+  //   await waitForElement(() => getByText("Monday"));
+
+  //   fireEvent.click(getByText("Tuesday"));
+
+  //   expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  // });
+  ////////
+  ////////
+
+  it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
+    const { container } = render(<Application />);
+    
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+    const appointment = getAllByTestId(container, "appointment")[0];
+
+    fireEvent.click(getByAltText(appointment, "Add"));
+
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { name: "Lydia Miller-Jones" }
+    });
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+    fireEvent.click(getByText(appointment, "Save"));
+
+    console.log(prettyDOM(container));
+  });
+
+
 });
-
-///////// ALTERNATIVE version of above, keep for reference ///////////
-
-// it("changes the schedule when a new day is selected", async () => {
-//   const { getByText } = render(<Application />);
-
-//   await waitForElement(() => getByText("Monday"));
-
-//   fireEvent.click(getByText("Tuesday"));
-
-//   expect(getByText("Leopold Silvers")).toBeInTheDocument();
-// });
-////////
-////////
